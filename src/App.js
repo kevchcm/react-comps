feaaa043f0aae415ddf62cbc6b87749aa2d709e5
kevchcm <kevin@maniak.com.mx>
2023-08-08 21:46:15 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,122 @@
-import logo from './logo.svg';
-import './App.css';
+import Button from './Button'
+import { GoBellFill, GoCloud } from 'react-icons/go'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+    return (
+        <div className="grid grid-cols-4">
+            <div className="space-y-4">
+                <div>
+                    <Button primary>
+                        <GoBellFill />
+                        Button 1
+                    </Button>
+                </div>
+                <div>
+                    <Button secondary>
+                        <GoCloud />
+                        Button 2
+                    </Button>
+                </div>
+                <div>
+                    <Button success>
+                        Button 3
+                    </Button>
+                </div>
+                <div>
+                    <Button warning>
+                        Button 4
+                    </Button>
+                </div>
+                <div>
+                    <Button danger>
+                        Button 5
+                    </Button>
+                </div>
+            </div>
+
+            <div className="space-y-4">
+                <div>
+                    <Button primary rounded>
+                        Button 1
+                    </Button>
+                </div>
+                <div>
+                    <Button secondary rounded>
+                        Button 2
+                    </Button>
+                </div>
+                <div>
+                    <Button success rounded>
+                        Button 3
+                    </Button>
+                </div>
+                <div>
+                    <Button warning rounded>
+                        Button 4
+                    </Button>
+                </div>
+                <div>
+                    <Button danger rounded>
+                        Button 5
+                    </Button>
+                </div>
+            </div>
+
+            <div className="space-y-4">
+                <div>
+                    <Button primary rounded outline>
+                        Button 1
+                    </Button>
+                </div>
+                <div>
+                    <Button secondary rounded outline>
+                        Button 2
+                    </Button>
+                </div>
+                <div>
+                    <Button success rounded outline>
+                        Button 3
+                    </Button>
+                </div>
+                <div>
+                    <Button warning rounded outline>
+                        Button 4
+                    </Button>
+                </div>
+                <div>
+                    <Button danger rounded outline>
+                        Button 5
+                    </Button>
+                </div>
+            </div>
+
+            <div className="space-y-4">
+                <div>
+                    <Button primary rounded>
+                        Button 1
+                    </Button>
+                </div>
+                <div>
+                    <Button secondary rounded>
+                        Button 2
+                    </Button>
+                </div>
+                <div>
+                    <Button success rounded>
+                        Button 3
+                    </Button>
+                </div>
+                <div>
+                    <Button warning rounded>
+                        Button 4
+                    </Button>
+                </div>
+                <div>
+                    <Button danger rounded>
+                        Button 5
+                    </Button>
+                </div>
+            </div>
+        </div>
+    )
 }
-
-export default App;
